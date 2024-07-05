64c9e1e89064e480a1b198e02025331210cd384c
liudaodan <liudaodanoo@126.com>
2024-07-05 15:05:48 +0800

    feat(big-react): 实现JSX，JSX的打包以及调试

---
+++ big-react/packages/react/src/jsx.ts
@@ -0,0 +1,105 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import {
+	Type,
+	Key,
+	Ref,
+	Props,
+	ReactElementType,
+	ElementType
+} from 'shared/ReactTypes';
+
+// ReactElement
+const ReactElement = function (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElementType {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'KaSong'
+	};
+
+	return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	let key: Key = null;
+	let ref: Ref = null;
+	const props: Props = {};
+
+	// 处理config
+	// 将config对象非原型中的属性复制到props中
+	// 对于特殊的属性名key、ref特殊处理
+	for (const prop in config) {
+		const val = config[prop];
+
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = '' + val;
+			}
+			continue;
+		}
+
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	// 处理chilren
+	// 将children复制到props中
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = (type: ElementType, config: any) => {
+	let key: Key = null;
+	let ref: Ref = null;
+	const props: Props = {};
+
+	// 处理config
+	// 将config对象非原型中的属性复制到props中
+	// 对于特殊的属性名key、ref特殊处理
+	for (const prop in config) {
+		const val = config[prop];
+
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = '' + val;
+			}
+			continue;
+		}
+
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	return ReactElement(type, key, ref, props);
+};
---
+++ big-react/packages/shared/ReactSymbols.ts
@@ -0,0 +1,6 @@
+// 是否支持Symbol
+const supportSymbol = typeof Symbol === 'function' && Symbol.for;
+
+export const REACT_ELEMENT_TYPE = supportSymbol
+	? Symbol.for('react.element')
+	: 0xeac7;
